0ef1749cbd2b5652e8b5220ccd39e83d68e4e57e
Dylan07c <djc0049@mix.wvu.edu>
2022-01-23 18:50:27 -0500

    Added features to first navbar

---
+++ src/app/navigation/firstnavbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'mn-firstnavbar',
+    templateUrl: 'firstnavbar.component.html',
+    styleUrls: ['./firstnavbar.component..css']
+})
+export class FirstNavBarComponent{
+
+
+
+}
