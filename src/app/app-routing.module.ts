4b3cfecfb62f9dcf0388c842103715541482669a
dylan cunningham <djc0049@mix.wvu.edu>
2022-02-23 14:02:47 -0500

    Expanded and refined piano and guitar pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { BrassMusicLayoutComponent } from "./category-page/brass-music-layout.component";
 import { GuitarMusicLayoutComponent } from "./category-page/guitar-music-layout.component";
 import { PianoMusicLayoutComponent } from "./category-page/piano-music-layout.component";
 import { VoiceMusicLayoutComponent } from "./category-page/voice-music-layout.component";
@@ -14,7 +13,6 @@ const routes:Routes = [
     {path: 'guitar-music', component: GuitarMusicLayoutComponent},
     {path: 'voice-music', component: VoiceMusicLayoutComponent},
     {path: 'woodwind-music', component: WoodwindMusicLayoutComponent},
-    {path: 'brass-music', component: BrassMusicLayoutComponent},
     {path: 'home,', component: HomeLayoutComponent}
 ];
 
--- src/app/category-page/mock-sm-card-list.ts
+++ src/app/category-page/mock-sm-card-list.ts
@@ -1,15 +1,22 @@
 export const mock_sm_card_list = [
     {
-        img: "\assets\Health_Benefits_Playing_Instrument.jpg",
+        img: "\assets\MN0225974.gif",
         title: "Driver's License",
         artist: "Olivia Rodrigo",
         inst: "Piano/Vocal/Chords, Singer Pro"
     },
 
     {
-        img: "\assets\Health_Benefits_Playing_Instrument.jpg",
-        title: "Say yes",
+        img: "src\assets\MN0229052.gif",
+        title: "Willow",
         artist: "Taylor Swift",
         inst: "Piano/Vocal/Chords, Singer Pro"
+    },
+
+    {
+        img: "\assets\MN0115783.gif",
+        title: "Hallelujah",
+        artist: "Leonard Cohen",
+        inst: "Big Note, Easy Piano"
     },
 ];
